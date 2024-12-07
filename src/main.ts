d296f07584886371cde6ecfea7c22ea91bd6b104
ana-sher <nshcherbakovaa@gmail.com>
2024-12-07 13:57:00 +0100

    Change vercel max duration

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+export const maxDuration = 50;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
