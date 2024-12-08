2e2798af06e20bc80c1d6ec7c0be06825ed0b3fc
ana-sher <nshcherbakovaa@gmail.com>
2024-12-08 18:46:29 +0100

    revert, update packages

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
 import { AppService } from './app.service';
 import { CreatorModule } from './creator/creator.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), CreatorModule],
+  imports: [CreatorModule],
   providers: [AppService],
 })
 export class AppModule {}
